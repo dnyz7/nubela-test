6f2c5c9c1b46659f308028b26ab881d5239c46a0
Denny Husniansyah <denny.husniansyah@gmail.com>
2023-05-05 04:01:54 +0700

    update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import sys
 import os
 import json
 
-#server_address = '/Users/dennyhusniansyah/development/py/landx-test/socks.sock'
+# server_address = '/Users/dennyhusniansyah/development/py/landx-test/socks.sock'
 
-if len(sys.argv)>1:
+if len(sys.argv) > 1:
     server_address = sys.argv[1]
 
     # Make sure the socket does not already exist
@@ -19,55 +19,49 @@ if len(sys.argv)>1:
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
     # Bind the socket to the port
-    print('starting up on %s' % server_address)
+    print("starting up on %s" % server_address)
     sock.bind(server_address)
 
     sock.listen(1)
 
     while True:
         # Wait for a connection
-        print('waiting for a connection')
+        print("waiting for a connection")
         connection, client_address = sock.accept()
         try:
-            #print('connection from', client_address)
+            # print('connection from', client_address)
 
-            data = connection.recv(128)
+            data = connection.recv(1024)
             if data:
-                print('received : "%s"' % data.decode('utf-8'))
-                datadecode = data.decode('utf-8')
-                storejson = datadecode.split('\n')
-                print('split : "%s"' % storejson)
-                
-                # cleanjson = [i for i in storejson if i]
-                #print('clean : "%s"' % cleanjson)
-
-                pulljson = []
-                listjson = {}
-                finaljson = '{"id": 42, "result": {"message": "Hello"}}'
-
-                # for idx, i in enumerate(cleanjson):
-                #     #print('loop :',idx, i)
-                #     convjson = json.loads(i)
-                #     #print('convert :',convjson)
-                #     xid = convjson['id']
-                    
-
-                #     #print('pulljson : ',pulljson)
-                #     listjson[xid]=pulljson
-                #     finaljson = finaljson + json.dumps(listjson) + '\n'
-                #     pulljson = []
-                #     listjson = {}
-                
-                
-                # print(finaljson)
-                connection.sendall(finaljson.encode('utf-8'))
-            else:
-                    print ('no data from', client_address)
+                print('received : "%s"' % data.decode("utf-8"))
+                datadecode = data.decode("utf-8")
+                storejson = datadecode.split("\n")
+                request_json = json.loads(datadecode)
+                if (
+                    "id" in request_json
+                    and "method" in request_json
+                    and "params" in request_json
+                ):
+                    if request_json["method"] == "echo":
+                        response = {
+                            "id": request_json["id"],
+                            "result": request_json["params"],
+                        }
+                        connection.send(json.dumps(response).encode())
+                    else:
+                        print("disconnected")
+                        break
+                else:
+                    print("disconnected")
                     break
-                
+
+            else:
+                print("no data from", client_address)
+                break
+
         finally:
+            # pass
             # Clean up the connection
             connection.close()
 else:
-    print ('no path sock file')
-    
+    print("no path sock file")
